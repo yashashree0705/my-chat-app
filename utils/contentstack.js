483f8f1093f0c51618b93ddc838987bf6bf73717
yashashree0705 <166916867+yashashree0705@users.noreply.github.com>
2025-09-06 18:36:54 +0530

    contentstack.js

---
+++ utils/contentstack.js
@@ -0,0 +1,9 @@
+import * as contentstack from "contentstack";
+
+const Stack = contentstack.Stack({
+  api_key: process.env.CONTENTSTACK_API_KEY,
+  delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN,
+  environment: process.env.CONTENTSTACK_ENVIRONMENT,
+});
+
+export default Stack;
